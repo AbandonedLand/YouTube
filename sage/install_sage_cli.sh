9491c761136795ea03419a671383aa8afa41e7dc
The Mayor of Abandoned Land <110296223+AbandonedLand@users.noreply.github.com>
2025-01-24 16:50:19 -0600

    Installing Sage

---
+++ sage/install_sage_cli.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+sudo apt install nodejs npm cmake libwebkit2gtk-4.1-dev build-essential curl wget file libxdo-dev libssl-dev libayatana-appindicator3-dev librsvg2-dev clang libc6-dev
+sudo npm install -g pnpm@latest-10
+
+# Restart server
+
+cargo install --git https://github.com/xch-dev/sage sage-cli --rev v0.9.4

333ece1516a192fe1da407695f9cb1972b227bb7
The Mayor <110296223+AbandonedLand@users.noreply.github.com>
2025-08-21 08:47:01 -0500

    Update install_sage_cli.sh

--- sage/install_sage_cli.sh
+++ sage/install_sage_cli.sh
@@ -5,4 +5,4 @@ sudo npm install -g pnpm@latest-10
 
 # Restart server
 
-cargo install --git https://github.com/xch-dev/sage sage-cli --rev v0.9.5
+cargo install --git https://github.com/xch-dev/sage sage-cli --rev v0.12.1
